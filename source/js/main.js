7418bc8c601583f69bd01433b04afbaba74e48a5
PoushnayaA <poushnaya@bk.ru>
2023-12-26 01:41:33 +0300

    Add hero and lots-block

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 
+import {initLotsSlider} from './modules/sliders/init-lots-slider';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,9 +16,10 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+
+    initLotsSlider();
+
     initModals();
     const form = new Form();
     window.form = form;
@@ -26,25 +29,25 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // ---------------------------------
 
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+const buttonCity = document.querySelector('[data-button="change-city"]');
+buttonCity.addEventListener('click', function () {
+  document.querySelector('.navigation__select-list').classList.toggle('visually-hidden');
+  document.querySelector('.navigation__select-button').classList.toggle('navigation__select-button--open-nav');
+});
 
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
+document.querySelector('.navigation__select-list').addEventListener('click', function (evt) {
+  document.querySelector('.navigation__select-list').classList.add('visually-hidden');
+  document.querySelector('.navigation__select-item--active').classList.remove('navigation__select-item--active');
+  buttonCity.classList.remove('navigation__select-button--open-nav');
+  if (evt.target.matches('.navigation__select-item')) {
+    evt.target.classList.add('navigation__select-item--active');
+    buttonCity.querySelector('div').textContent = evt.target.textContent;
+  }
+});
 
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
+const buttonNavigation = document.querySelector('[data-button="navigation"]');
+buttonNavigation.addEventListener('click', function () {
+  buttonNavigation.classList.toggle('navigation__button--active');
+  document.querySelector('.navigation__list--desktop').classList.toggle('navigation__list--open');
+});
 
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
---
+++ source/js/modules/sliders/init-lots-slider.js
@@ -0,0 +1,43 @@
+// const sliderLots = document.querySelector('[data-slider="lots-swiper"]');
+// // const buttonNext = document.querySelector('[data-jury="jury-button-next"]');
+// // const buttonPrev = document.querySelector('[data-jury="jury-button-prev"]');
+
+// const initLotsSlider = () => {
+//   if (sliderLots) {
+//     // eslint-disable-next-line
+//     new Swiper(sliderLots, {
+//       // navigation: {
+//       //   nextEl: buttonNext,
+//       //   prevEl: buttonPrev,
+//       // },
+
+//       // pagination: {
+//       //   el: '.swiper-pagination',
+//       // },
+
+//       loop: true,
+
+//       breakpoints: {
+//         // 1366: {
+//         //   initialSlide: 0,
+//         //   slidesPerView: 4,
+//         //   spaceBetween: 40,
+//         // },
+
+//         768: {
+//           initialSlide: 2,
+//           slidesPerView: 2,
+//           spaceBetween: 32,
+//         },
+
+//         320: {
+//           initialSlide: 0,
+//           slidesPerView: 1,
+//           // spaceBetween: 20,
+//         },
+//       },
+//     });
+//   }
+// };
+
+// export {initLotsSlider};
