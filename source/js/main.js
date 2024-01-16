88804f7cc5df69e84a62d4140a1c632f205863f3
PoushnayaA <poushnaya@bk.ru>
2024-01-16 13:10:12 +0300

    Final 1

--- source/js/main.js
+++ source/js/main.js
@@ -13,6 +13,52 @@ import { initAccordions } from './modules/accordion/init-accordion';
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
 
+
+
+  const buttonRegistration = document.querySelector('[data-button="registration"]');
+  const buttonRecoveryPassword = document.querySelector('[data-button="recovery-password"]');
+  const buttonSuccessRecoveryPassword = document.querySelector('[data-button="send"]');
+
+  if (buttonRegistration) {
+    buttonRegistration.addEventListener('click', function () {
+      document.querySelector('[data-modal="success-registration"]').classList.remove('visually-hidden');
+    });
+  }
+
+  const buttonCloseRegistration = document.querySelector('[data-modal="success-registration"]');
+  if (buttonCloseRegistration) {
+    buttonCloseRegistration.querySelector('[data-button="close-modal"]').addEventListener('click', function () {
+      document.querySelector('[data-modal="success-registration"]').classList.add('visually-hidden');
+    });
+  }
+
+  if (buttonRecoveryPassword) {
+    buttonRecoveryPassword.addEventListener('click', function () {
+      document.querySelector('[data-modal="password-recovery"]').classList.remove('visually-hidden');
+    });
+  }
+
+  const buttonClosePasswordRecovery = document.querySelector('[data-modal="password-recovery"]');
+  if (buttonClosePasswordRecovery) {
+    buttonClosePasswordRecovery.querySelector('[data-button="close-modal"]').addEventListener('click', function () {
+      document.querySelector('[data-modal="password-recovery"]').classList.add('visually-hidden');
+    });
+  }
+
+  if (buttonSuccessRecoveryPassword) {
+    buttonSuccessRecoveryPassword.addEventListener('click', function () {
+      document.querySelector('[data-modal="password-recovery"]').classList.add('visually-hidden');
+      document.querySelector('[data-modal="success-password-recovery"]').classList.remove('visually-hidden');
+    });
+  }
+
+  const buttonCloseSuccessPasswordRecovery = document.querySelector('[data-modal="success-password-recovery"]');
+  if (buttonCloseSuccessPasswordRecovery) {
+    buttonCloseSuccessPasswordRecovery.querySelector('[data-button="close-modal"]').addEventListener('click', function () {
+      document.querySelector('[data-modal="success-password-recovery"]').classList.add('visually-hidden');
+    });
+  }
+
   // function getTimeRemaining(endtime) {
   //   var t = Date.parse(endtime) - Date.parse(new Date());
   //   var seconds = Math.floor((t / 1000) % 60);
@@ -68,8 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   const dateMask = document.getElementById('birth-date');
-  if (dateMask)
-   {
+  if (dateMask) {
     IMask(dateMask, {
       mask: Date,
       // lazy: false,
@@ -92,11 +137,9 @@ window.addEventListener('DOMContentLoaded', () => {
           from: 1,
           to: 31,
         },
-      }
+      },
     });
-   }
-
-
+  };
 
   const loginForm = document.querySelector('.log-in__navigation-item--login');
   const registrationForm = document.querySelector('.log-in__navigation-item--registration');
@@ -105,7 +148,7 @@ window.addEventListener('DOMContentLoaded', () => {
     loginForm.addEventListener('click', function () {
       document.querySelector('.log-in__navigation-item--active').classList.remove('log-in__navigation-item--active');
       loginForm.classList.add('log-in__navigation-item--active');
-      document.querySelector('.log-in__form--main').classList.add('.log-in__form--active');
+      document.querySelector('.log-in__form--main').classList.add('log-in__form--active');
       document.querySelector('.log-in__form--main').classList.remove('visually-hidden');
       document.querySelector('.log-in__form--registration').classList.add('visually-hidden');
     });
@@ -155,18 +198,21 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   // window.addEventListener('load', () => {
-    setTimeout(initLotsSlider(), 1000);
-    setTimeout(initNewsSlider(), 1000);
-    setTimeout(initAccordions(), 1000);
-    setTimeout(initPhotoSlider(), 1000);
+  setTimeout(initLotsSlider(), 1000);
+  setTimeout(initNewsSlider(), 1000);
+  setTimeout(initAccordions(), 1000);
+  setTimeout(initPhotoSlider(), 1000);
 
-    const btnText = document.querySelector('.lots__more-button');
+  const btnText = document.querySelector('.lots__more-button');
+  if (btnText) {
     btnText.addEventListener('click', function () {
       document.querySelectorAll('.lots__photo-item--hidden').forEach(function (item) {
         item.classList.toggle('lots__photo-item--hidden');
         btnText.classList.toggle('visually-hidden');
       });
     });
+  }
+
   // });
 });
 
@@ -192,24 +238,35 @@ const buttonNavigation = document.querySelector('[data-button="navigation"]');
 buttonNavigation.addEventListener('click', function () {
   buttonNavigation.classList.toggle('navigation__button--active');
   document.querySelector('.navigation__list--desktop').classList.toggle('navigation__list--open');
-  document.querySelector('main').classList.toggle('main--dark');
+  document.querySelector('.page').classList.toggle('page--dark');
 });
 
 const buttonFilter = document.querySelector('[data-button="change-filter"]');
-buttonFilter.addEventListener('click', function () {
-  document.querySelector('.filter-sorting__select-list').classList.toggle('visually-hidden');
-  document.querySelector('.filter-sorting__select-button').classList.toggle('filter-sorting__select-button--open-nav');
+if (buttonFilter) {
+  buttonFilter.addEventListener('click', function () {
+    document.querySelector('.filter-sorting__select-list').classList.toggle('visually-hidden');
+    document.querySelector('.filter-sorting__select-button').classList.toggle('filter-sorting__select-button--open-nav');
+  });
+}
+
+if (document.querySelector('.filter-sorting__select-list')) {
+  document.querySelector('.filter-sorting__select-list').addEventListener('click', function (evt) {
+    document.querySelector('.filter-sorting__select-list').classList.add('visually-hidden');
+    document.querySelector('.filter-sorting__select-item--active').classList.remove('filter-sorting__select-item--active');
+    buttonFilter.classList.remove('filter-sorting__select-button--open-nav');
+    if (evt.target.matches('.filter-sorting__select-item')) {
+      evt.target.classList.add('filter-sorting__select-item--active');
+      document.querySelector('.filter-sorting__select').dataset.filter = evt.target.dataset.filter;
+    }
+  });
+}
+
+document.querySelectorAll('.lots__item').forEach(function (lot) {
+  lot.querySelector('.favorite').addEventListener('click', function () {
+    lot.classList.toggle('lots__item--favorite');
+  });
 });
 
-document.querySelector('.filter-sorting__select-list').addEventListener('click', function (evt) {
-  document.querySelector('.filter-sorting__select-list').classList.add('visually-hidden');
-  document.querySelector('.filter-sorting__select-item--active').classList.remove('filter-sorting__select-item--active');
-  buttonFilter.classList.remove('filter-sorting__select-button--open-nav');
-  if (evt.target.matches('.filter-sorting__select-item')) {
-    evt.target.classList.add('filter-sorting__select-item--active');
-    document.querySelector('.filter-sorting__select').dataset.filter = evt.target.dataset.filter;
-  }
-});
 
 document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
   const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
@@ -227,12 +284,22 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
   dropDownListItems.forEach(function (listItem) {
     listItem.addEventListener('click', function (e) {
       e.stopPropagation();
-      dropDownBtn.innerText = this.innerText;
+      dropDownBtn.innerText = listItem.innerText;
       // dropDownBtn.focus();
-      dropDownInput.value = this.dataset.value;
+      dropDownInput.dataset.option = listItem.dataset.value;
       dropDownList.classList.remove('dropdown__list--visible');
       dropDownBtn.classList.remove('dropdown__button--active');
+
+      if (dropDownInput.dataset.option !== 'default') {
+        dropDownBtn.style.color = "#292929";
+        document.querySelector('.filter__reset').classList.remove('visually-hidden');
+        document.querySelector('.filter__submit').classList.add('filter__submit--active');
+      }
     });
+  });
+
+  document.querySelector('.filter__reset').addEventListener('click', function () {
+    location.reload();
   });
 
   // Клик снаружи дропдауна. Закрыть дропдаун
@@ -251,3 +318,141 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     }
   });
 });
+
+if (document.querySelector('.price-card__reload-button')) {
+  document.querySelector('.price-card__reload-button').addEventListener('click', function () {
+    location.reload();
+  });
+}
+
+if (document.querySelector('.price-card__reload-button--account')) {
+  document.querySelector('.price-card__reload-button--account').addEventListener('click', function () {
+    location.reload();
+  });
+}
+
+const emailEnter = document.querySelector('.log-in__form--main').querySelector('.connection__field--email');
+const passwordEnter = document.querySelector('.log-in__form--main').querySelector('.connection__field--password');
+const EMAIL_REGEXP = /^[^@\s]+@[^@\s]+\.[^@\s]+$/iu;
+
+if (emailEnter && passwordEnter) {
+  passwordEnter.addEventListener('input', function () {
+    if (passwordEnter.value !== "" && emailEnter.value !== "") {
+      document.querySelector('.log-in__form--main').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--main').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  });
+  emailEnter.addEventListener('input', function () {
+    if (passwordEnter.value !== "" && emailEnter.value !== "") {
+      document.querySelector('.log-in__form--main').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--main').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+    if (!EMAIL_REGEXP.test(email.value)) {
+      document.querySelector('.error-message').classList.remove('visually-hidden');
+      document.querySelector('.connection__field--email').classList.add('connection__field--error');
+    } else {
+      document.querySelector('.error-message').classList.add('visually-hidden');
+      document.querySelector('.connection__field--email').classList.remove('connection__field--error');
+    }
+  }
+  );
+}
+
+const emailReg = document.querySelector('.log-in__form--registration').querySelector('.connection__field--reg-email');
+const passwordReg = document.querySelector('.log-in__form--registration').querySelector('.connection__field--reg-password');
+const passwordRepeat = document.querySelector('.log-in__form--registration').querySelector('.connection__field--reg-password-repeat');
+
+const name = document.querySelector('.log-in__form--registration').querySelector('.connection__field--name');
+const dateBirth = document.querySelector('.log-in__form--registration').querySelector('.connection__field--birth-date');
+const phone = document.querySelector('.log-in__form--registration').querySelector('.connection__field--phone');
+
+const checkboxAgreementLabel = document.querySelector('.checkbox--agreement');
+const checkboxNotificationLabel = document.querySelector('.checkbox--notification');
+const checkboxAgreement = document.querySelector('.checkbox--agreement').querySelector('.checkbox__input');
+const checkboxNotification = document.querySelector('.checkbox--notification').querySelector('.checkbox__input');
+
+if (emailReg && passwordReg && passwordRepeat && name && dateBirth && phone && checkboxAgreement && checkboxNotification) {
+  passwordReg.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  });
+  emailReg.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+    if (!EMAIL_REGEXP.test(emailReg.value)) {
+      document.querySelector('.error-message').classList.remove('visually-hidden');
+      document.querySelector('.connection__field--reg-email').classList.add('connection__field--error');
+    } else {
+      document.querySelector('.error-message').classList.add('visually-hidden');
+      document.querySelector('.connection__field--reg-email').classList.remove('connection__field--error');
+    }
+  }
+  );
+  passwordRepeat.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  }
+  );
+  name.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  }
+  );
+  dateBirth.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  }
+  );
+  phone.addEventListener('input', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  }
+  );
+  checkboxAgreementLabel.addEventListener('click', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  });
+  checkboxNotificationLabel.addEventListener('click', function () {
+    if (checkboxNotification.checked && checkboxAgreement.checked && passwordReg.value !== "" && emailReg.value !== "" && passwordRepeat.value !== "" && name.value !== "" && dateBirth.value !== "" && phone.value.length === 18) {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.add('connection__button--active');
+    } else {
+      document.querySelector('.log-in__form--registration').querySelector('.connection__button--submit').classList.remove('connection__button--active');
+    }
+  });
+}
+
+const emailRecovery = document.querySelector('.connection__field--recovery');
+if (emailRecovery) {
+  emailRecovery.addEventListener('input', function () {
+    if (emailRecovery.value !== "") {
+      document.querySelector('.modal__button-send').classList.add('modal__button-send--active');
+    } else {
+      document.querySelector('.modal__button-send').classList.remove('modal__button-send--active');
+    }
+  });
+}
+
+// document.querySelector('.authorization').classList.add('visually-hidden');
